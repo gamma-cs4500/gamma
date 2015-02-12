05ea15859732cf035cd20ddfc2bc4d0b6bc8a4f7
scottk <scottk@ccs.neu.edu>
2015-02-11 22:07:12 -0500

    Checking in broken PlacesFragment to use on Desktop

--- Gather_Android/app/src/main/java/com/gatherfriends/gather/NewEventWizard/NewEventActivity.java
+++ Gather_Android/app/src/main/java/com/gatherfriends/gather/NewEventWizard/NewEventActivity.java
@@ -88,14 +88,16 @@ public class NewEventActivity extends ActionBarActivity {
             switch (position) {
                 case 0:
                     return new NameDateTimeFragment();
+                case 1:
+                    return new PlacesFragment();
             }
             return null;
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 1;
+            // Show 2 total pages.
+            return 2;
         }
 
         @Override
@@ -103,41 +105,10 @@ public class NewEventActivity extends ActionBarActivity {
             switch (position) {
                 case 0:
                     return "New Date & Time";
+                case 1:
+                    return "Places";
             }
             return null;
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_new_event, container, false);
-            return rootView;
         }
     }
 
---
+++ Gather_Android/app/src/main/java/com/gatherfriends/gather/NewEventWizard/PlacesFragment.java
@@ -0,0 +1,179 @@
+package com.gatherfriends.gather.NewEventWizard;
+
+import android.app.ListFragment;
+import android.location.LocationProvider;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.gatherfriends.gather.R;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.ByteArrayBuffer;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlacesFragment extends ListFragment {
+
+    ArrayList venuesList;
+
+    // the google key
+
+    // ============== YOU SHOULD MAKE NEW KEYS ====================//
+    final String GOOGLE_KEY = getString(R.string.places_api_key);
+
+    // we will need to take the latitude and the logntitude from a certain point
+    // this is the center of New York
+    final String latitude = "40.7463956";
+    final String longtitude = "-73.9852992";
+
+    //TODO: figure out location. for now, just static.
+    //private GoogleApiClient mGoogleApiClient;
+
+    ArrayAdapter myAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        new googleplaces().execute();
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_name_date_time, container, false);
+    }
+
+    private class googleplaces extends AsyncTask {
+
+        String temp;
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            temp = makeCall("https://maps.googleapis.com/maps/api/place/search/json?location=" + latitude + "," + longtitude + "&radius=100&sensor=true&key=" + GOOGLE_KEY);
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            // we can start a progress bar here
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (temp == null) {
+                // we have an error to the call
+                // we can also stop the progress bar
+            } else {
+                // all things went right
+
+                // parse Google places search result
+                venuesList = parseGoogleParse(temp);
+
+                List listTitle = new ArrayList();
+
+                for (int i = 0; i < venuesList.size(); i++) {
+                    // make a list of the venus that are loaded in the list.
+                    // show the name, the category and the city
+                    listTitle.add(i, venuesList.get(i).getName() + "\nOpen Now: " + venuesList.get(i).getOpenNow() + "\n(" + venuesList.get(i).getCategory() + ")");
+                }
+
+                // set the results to the list
+                // and show them in the xml
+                myAdapter = new ArrayAdapter(PlacesFragment.this, R.layout.row_layout, R.id.listText, listTitle);
+                setListAdapter(myAdapter);
+            }
+        }
+    }
+
+    public static String makeCall(String url) {
+
+        // string buffers the url
+        StringBuffer buffer_string = new StringBuffer(url);
+        String replyString = "";
+
+        // instanciate an HttpClient
+        HttpClient httpclient = new DefaultHttpClient();
+        // instanciate an HttpGet
+        HttpGet httpget = new HttpGet(buffer_string.toString());
+
+        try {
+            // get the responce of the httpclient execution of the url
+            HttpResponse response = httpclient.execute(httpget);
+            InputStream is = response.getEntity().getContent();
+
+            // buffer input stream the result
+            BufferedInputStream bis = new BufferedInputStream(is);
+            ByteArrayBuffer baf = new ByteArrayBuffer(20);
+            int current = 0;
+            while ((current = bis.read()) != -1) {
+                baf.append((byte) current);
+            }
+            // the result as a string is ready for parsing
+            replyString = new String(baf.toByteArray());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(replyString);
+
+        // trim the whitespaces
+        return replyString.trim();
+    }
+
+    private static ArrayList parseGoogleParse(final String response) {
+
+        ArrayList temp = new ArrayList();
+        try {
+
+            // make an jsonObject in order to parse the response
+            JSONObject jsonObject = new JSONObject(response);
+
+            // make an jsonObject in order to parse the response
+            if (jsonObject.has("results")) {
+
+                JSONArray jsonArray = jsonObject.getJSONArray("results");
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    GooglePlace poi = new GooglePlace();
+                    if (jsonArray.getJSONObject(i).has("name")) {
+                        poi.setName(jsonArray.getJSONObject(i).optString("name"));
+                        poi.setRating(jsonArray.getJSONObject(i).optString("rating", " "));
+
+                        if (jsonArray.getJSONObject(i).has("opening_hours")) {
+                            if (jsonArray.getJSONObject(i).getJSONObject("opening_hours").has("open_now")) {
+                                if (jsonArray.getJSONObject(i).getJSONObject("opening_hours").getString("open_now").equals("true")) {
+                                    poi.setOpenNow("YES");
+                                } else {
+                                    poi.setOpenNow("NO");
+                                }
+                            }
+                        } else {
+                            poi.setOpenNow("Not Known");
+                        }
+                        if (jsonArray.getJSONObject(i).has("types")) {
+                            JSONArray typesArray = jsonArray.getJSONObject(i).getJSONArray("types");
+
+                            for (int j = 0; j < typesArray.length(); j++) {
+                                poi.setCategory(typesArray.getString(j) + ", " + poi.getCategory());
+                            }
+                        }
+                    }
+                    temp.add(poi);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList();
+        }
+        return temp;
+
+    }
+}
