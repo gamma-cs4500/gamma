89546be87c38d3d2a8fccb221acf9cb74f85388e
scottk <scottk@ccs.neu.edu>
2015-02-25 20:17:47 -0500

    checking

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(cors);
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
