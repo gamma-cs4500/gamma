6fefa41a2b312133f77ba6a773b983152daac67c
Basel Alhusseini <Basel@Basels-MacBook-Air.local>
2015-04-22 00:47:56 -0400

    stuff

--- test/test.js
+++ test/test.js
@@ -89,3 +89,122 @@ describe('LogoutFlow', function () {
     });
   });
 });
+
+// New Game Creation Tests
+describe('NewGame', function () {
+  this.timeout(5000);
+  
+  // Run this before each test in NewGame
+  beforeEach(function (done) {
+    // Create fake users
+    models.sequelize.sync({force: true, logging: false})
+        .then(function() {
+            var jamel = {username: 'jamel', password: 'jamel'};
+            var basel = {username: 'basel@husky.neu.edu', password: 'basel'};
+            var ryan = {username: 'ryan', password: 'ryan'};
+            models.User.bulkCreate([jamel, basel, ryan]);
+        })
+        .then(done);
+  });
+
+  describe('Create new private game', function() {
+    it('Should successfully create new private game', function(done) {
+      request(app)
+        .post('/game/')
+        .send({'name': 'Hardcore', 'shortDesc': 'its Hardcore', 'longDesc': 'its so very Hardocre',
+         'visibility': 'private', 'GenreId': '5', 'PlatformId': '1', 'LicenseId': '1'})
+        .res.redirect('/game/:id') // Must figure out proper way to catch a redirect in order to test success
+    });
+  });
+});
+
+// Rating Tests
+describe('Rating', function () {
+  this.timeout(5000);
+  
+  // Run this before each test in Rating
+  beforeEach(function (done) {
+    // Create fake users
+    models.sequelize.sync({force: true, logging: false})
+        .then(function() {
+            var jamel = {username: 'jamel', password: 'jamel'};
+            var basel = {username: 'basel@husky.neu.edu', password: 'basel'};
+            var ryan = {username: 'ryan', password: 'ryan'};
+            models.User.bulkCreate([jamel, basel, ryan]);
+
+            var game1 = {name: 'hardcore', shortDesc: 'its hardcore', longDesc: 'very hardcore',
+            visibility: 'private', GenreId: '5', PlatformId: '1', LicenseId: '1'};
+            models.Game.create([game1]);
+
+        })
+        .then(done);
+      });
+
+  describe('Give raiting to game', function() {
+    it ('Should successfully add new rating to game', function(done) {
+      request(app)
+      .post('/game/1/rating')
+      .send({'rating': '5', 'GameId': '1', 'UserId': '1'})
+      .expect()
+      // Figure out how to extract rating from database user last entry.
+    });
+  });
+
+  describe('Change game rating', function() {
+    it ('Should successfully add new rating to game', function(done) {
+      request(app)
+      // Figure out how to extract rating from database prior to user rating it
+      .post('/game/1/rating')
+      .send({'rating': '5', 'GameId': '1', 'UserId': '1'})
+      .expect()
+      // Figure out how to extract rating from database user last entry.
+    });
+  });
+
+  describe('Rating a game twice', function() {
+    it ('Should fail to add new rating to game', function(done) {
+      // Figure out how to extract rating from database prior to user rating it
+      request(app)
+      .post('/game/1/rating')
+      .send({'rating': '5', 'GameId': '1', 'UserId': '1'})
+      .expect()
+      // Figure out how to extract rating from database user last entry.
+    });
+  });
+});
+
+// Commenting Tests
+describe('Commenting', function () {
+  this.timeout(5000);
+  
+  // Run this before each test in Commenting
+  beforeEach(function (done) {
+    // Create fake users
+    models.sequelize.sync({force: true, logging: false})
+        .then(function() {
+            var jamel = {username: 'jamel', password: 'jamel'};
+            var basel = {username: 'basel@husky.neu.edu', password: 'basel'};
+            var ryan = {username: 'ryan', password: 'ryan'};
+            models.User.bulkCreate([jamel, basel, ryan]);
+
+            var game1 = {name: 'hardcore', shortDesc: 'its hardcore', longDesc: 'very hardcore',
+            visibility: 'private', GenreId: '5', PlatformId: '1', LicenseId: '1'};
+            models.Game.create([game1]);
+
+        })
+        .then(done);
+      });
+
+  describe('Add a comment', function() {
+    it ('Should successfully add new rating to game', function(done) {
+      request(app)
+      // Figure out how to extract rating from database prior to user rating it
+      .post('/game/1/comment')
+      .send({'comment': 'nice game', 'GameId': '1', 'UserId': '1'})
+      .expect()
+      // Figure out how to extract rating from database user last entry.
+    });
+  });
+});
+
+//Adding Collaborator
